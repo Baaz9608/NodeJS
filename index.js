a3fb2b2de66cc4a7355ecfb50793a194e9413c02
BaazShah606 <shahbazmd3002@gmail.com>
2023-12-31 11:43:41 +0530

    synchronous and synchronous handle

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const fs = require('fs');
-const path = require('path');
-const dirPath = path.join(__dirname, 'Crud');
-const filePath = `${dirPath}/apple.txt`
-// fs.writeFileSync(filePath, 'This a company ')
-// fs.readFile(filePath,'utf-8', (_, item)=>{
-//     console.log(item)
-// })
+let a = 10;
+let b = 20;
 
-// fs.appendFile(filePath, 'and this is the apple.txt file', (err)=>{
-//     if(!err) console.log('file is undated')
-// })
 
-fs.unlinkSync(`${dirPath}/fruit.txt`);
+
+new Promise((res, rej)=>{
+    setTimeout(()=>{
+        b = 90;
+        res(b);
+    },2000)
+}).then((data)=>{
+    b = data;
+    console.log(a+b)
+})
