5a908ac5da6e1c345a334c560a3249650f42f150
BaazShah606 <shahbazmd3002@gmail.com>
2023-12-31 12:44:00 +0530

    inserted html file and making default file by naming index.html

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express')
+const path = require('path')
+
+
 const app = express();
-app.get('', (_, res)=>{
-    res.send('Hello, this is home page')
-});
-app.get('/about', (req, res)=>{
-    console.log("data sent by user ->>>", req.query)
-    res.send(`this is ${req.query.name}'s about section`)
-});
-app.get('/help', (_, res)=>{
-    res.send({
-        name:'shahbaz',
-        email:'shaib@gmail.com'
-    })
-});
+const publicPath = path.join(__dirname, 'public');
+app.use(express.static(publicPath))
+// app.get('', (_, res)=>{
+//     res.send('Hello, this is home page')
+// });
+// app.get('/about', (req, res)=>{
+//     console.log("data sent by user ->>>", req.query)
+//     res.send(`this is ${req.query.name}'s about section`)
+// });
+// app.get('/help', (_, res)=>{
+//     res.send({
+//         name:'shahbaz',
+//         email:'shaib@gmail.com'
+//     })
+// });
 
 app.listen(3000)
