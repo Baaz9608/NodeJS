be14451d3d9fda9a78deaa857e1017102a0ab599
BaazShah606 <shahbazmd3002@gmail.com>
2023-12-31 12:27:17 +0530

    req and res

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const app = express();
 app.get('', (_, res)=>{
     res.send('Hello, this is home page')
 });
-app.get('/about', (_, res)=>{
-    res.send(' this is about page')
+app.get('/about', (req, res)=>{
+    console.log("data sent by user ->>>", req.query)
+    res.send(`this is ${req.query.name}'s about section`)
 });
 app.get('/help', (_, res)=>{
-    res.send(' this is help page')
+    res.send({
+        name:'shahbaz',
+        email:'shaib@gmail.com'
+    })
 });
 
 app.listen(3000)
