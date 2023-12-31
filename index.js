2be12b68575d2232fc1c58091e0d784b60d99c73
BaazShah606 <shahbazmd3002@gmail.com>
2024-01-01 00:07:37 +0530

    middlewares

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const path = require('path')
 
 const app = express();
 const publicPath = path.join(__dirname, 'public');
+
+const reqFilter = (req, res,next)=>{
+    if(!req.query.age){
+        res.send('Please provide age')
+    }
+    else if(req.query.age<18){
+        res.send('You can not access this page!!!')
+    }
+    else{
+        next()
+    }
+    
+}
+
+app.use(reqFilter);
+
 app.set('view engine', 'ejs');
 
 app.get('', (_, res)=>{
