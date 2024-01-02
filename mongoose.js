fb4593b980faa05f7e5dce1b3409dd9f6b024816
BaazShah606 <shahbazmd3002@gmail.com>
2024-01-02 20:39:27 +0530

    basic of mongoose

--- mongoose.js
+++ mongoose.js
@@ -1,1 +1,23 @@
-const mongodb = require('mongodb')
+ const mongoose = require('mongoose')
+
+ const main = async()=>{
+    await mongoose.connect('mongodb://localhost:27017/e-comm');
+    // schema validates the field in databases and define fields
+    const productSchema = new mongoose.Schema(
+        {
+            name: String,
+
+        }
+    );
+
+    // model basically schemas ko use ker k mongodb aur mongoose ko connect krta hai
+
+    const productsModel = mongoose.model('products', productSchema);
+    let data = new productsModel({name: 'm14'});
+    let result = await data.save();
+    console.log(result);
+}
+
+main();
+
+ 
