1bbe282436ea4ddca6166cbee7db6e60ee774e03
BaazShah606 <shahbazmd3002@gmail.com>
2024-01-02 10:51:08 +0530

    different ways to connect with database

--- mongodb.js
+++ mongodb.js
@@ -5,11 +5,28 @@ const url = 'mongodb://localhost:27017';
 const database = 'e-comm';
 const client = new MongoClient(url);
 
-async function getData(){
+// method 1
+async function dbConnect(){
     let result = await client.connect();
     let db = result.db(database);
-    let collection = db.collection('products');
-    let res = await collection.find({}).toArray();
-    console.log(res)
+    return db.collection('products');
+     
 }
-getData();
+
+module.exports = dbConnect;
+
+// method 2
+// async function getData(){
+//     let result = await client.connect();
+//     let db = result.db(database);
+//     let collection = db.collection('products');
+//     let res = await collection.find({}).toArray();
+//     console.log(res)
+// }
+
+// dbConnect().then((res)=>{
+//     console.log(res.find().toArray());
+// })
+
+// console.warn(dbConnect()) 
+
