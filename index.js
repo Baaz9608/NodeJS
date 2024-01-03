7b8adc6719ff049e225df8ce25fe2ae15c5f523d
BaazShah606 <shahbazmd3002@gmail.com>
2024-01-03 10:08:49 +0530

    inserted methods such as delete, put, get and post

--- index.js
+++ index.js
@@ -13,4 +13,25 @@ app.post('/create', async(req, res)=>{
     console.log(req.body) //req.body is in stream so we have to parse it with the help of middleware -> app.use(express.json())
     res.send('done');
 })
+
+app.get('/list',async(req, res)=>{
+    let data = await Product.find();
+    res.send(data);
+})
+
+app.delete('/delete/:_id',async(req, res)=>{
+    console.log(req.params);
+    let data = await Product.deleteOne(req.params);
+    res.send(data);
+})
+
+app.put('/update/:_id', async(req, res)=>{
+    console.log(req.params);
+    let data = await Product.updateOne(
+        req.params,
+        {$set: req.body}
+    )
+    res.send(data);
+})
+
 app.listen(3000)
