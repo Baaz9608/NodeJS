c84fd85af44c5e1e45060c322ee96d9d225f95e9
BaazShah606 <shahbazmd3002@gmail.com>
2024-01-03 10:37:58 +0530

    file upload with the help of multer

--- index.js
+++ index.js
@@ -1,53 +1,21 @@
 const express = require('express');
-require('./config');
-const Product = require('./product');
+const multer = require('multer');
 
-const app = express()
-app.use(express.json());
+const app = express();
 
-// SEARCH APIs
-app.get('/search/:key', async(req, res)=>{
-    console.log(req.params.key)
-    let data = await Product.find(
-        {
-            '$or':[
-                {'name': {$regex: req.params.key}},
-                {'brand': {$regex: req.params.key}}
-            ]
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function(req, file, cb){
+            cb(null,'uploads')
+        },
+        filename: function(req, file, cb){
+            cb(null, file.fieldname + '-' + Date.now()+'.jpg')
         }
-    );
-    res.send(data);
-})
+    })
+}).single('user_file')
 
-// METHODS
-
-app.post('/create', async(req, res)=>{
-
-    let data =new Product(req.body);
-    let result = await data.save();
-
-    console.log(req.body) //req.body is in stream so we have to parse it with the help of middleware -> app.use(express.json())
-    res.send('done');
-})
-
-app.get('/list',async(req, res)=>{
-    let data = await Product.find();
-    res.send(data);
-})
-
-app.delete('/delete/:_id',async(req, res)=>{
-    console.log(req.params);
-    let data = await Product.deleteOne(req.params);
-    res.send(data);
-})
-
-app.put('/update/:_id', async(req, res)=>{
-    console.log(req.params);
-    let data = await Product.updateOne(
-        req.params,
-        {$set: req.body}
-    )
-    res.send(data);
+app.post('/upload', upload, (req,res)=>{
+    res.send('file uploaded')
 })
 
 app.listen(3000)
