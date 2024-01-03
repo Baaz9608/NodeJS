3eb9d340d87b253b1367280b840db395f750d0d9
BaazShah606 <shahbazmd3002@gmail.com>
2024-01-03 10:47:04 +0530

    some function of os module learnt

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-const express = require('express');
-const multer = require('multer');
-
-const app = express();
-
-const upload = multer({
-    storage: multer.diskStorage({
-        destination: function(req, file, cb){
-            cb(null,'uploads')
-        },
-        filename: function(req, file, cb){
-            cb(null, file.fieldname + '-' + Date.now()+'.jpg')
-        }
-    })
-}).single('user_file')
-
-app.post('/upload', upload, (req,res)=>{
-    res.send('file uploaded')
-})
-
-app.listen(3000)
+const os = require('os');
+console.log(os.arch());
+console.log(os.freemem()/(1024*1024*1024));
+console.log(os.totalmem()/(1024*1024*1024));
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.userInfo());
